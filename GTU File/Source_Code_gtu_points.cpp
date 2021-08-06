547a78dff42f3de300269b02512d06f500527ba6
Naviz Khoja <116066_nk.beit20@ldceahm.gujgov.edu.in>
2021-08-06 15:28:21 +0530

    Stat updated

--- GTU File/Source_Code_gtu_points.cpp
+++ GTU File/Source_Code_gtu_points.cpp
@@ -1,23 +1,23 @@
 // Author.... Rohan R Lahori
 
 #include <iostream>
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 #include <algorithm>
 using namespace std;
 int main()
 {
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
-        int x,y,z;
-        cin>>x>>y>>z;
-        if(x>=40 && y>=50 && z>=10)
+        int x, y, z;
+        cin >> x >> y >> z;
+        if (x >= 40 && y >= 50 && z >= 10)
         {
-            cout<<"YES"<<"\n";
+            cout << "YES" << "\n";
         }
-        else{
-            cout<<"NO"<<"\n";   
+        else {
+            cout << "NO" << "\n";
         }
     }
 }
